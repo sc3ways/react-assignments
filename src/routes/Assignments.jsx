103018213562506637b34d3714ccc849d06fcf12
SunilG <sc3ways@gmail.com>
2024-12-23 07:09:16 +0530

    Add assignment lists in Home page

--- src/routes/Assignments.jsx
+++ src/routes/Assignments.jsx
@@ -1,22 +1,7 @@
 import AssignmentCard from "../components/AssignmentCard";
+import assignments from "../data/assignmentList.json";
 
 const Assignments = () => {
-  const assignments = [
-    {
-      id: "001",
-      img: "images/calculator.png",
-      title: "Calculator",
-      text: "Basic calculators can do only addition, subtraction, multiplication and division mathematical calculations.",
-      url: "/react-assignment/calculator",
-    },
-    {
-      id: "002",
-      img: "images/todo-img.png",
-      title: "Todo List",
-      text: "List everything that you have to do, with the most important tasks at the top, and the least important tasks at the bottom. ",
-      url: "/react-assignment/todolist",
-    },
-  ];
   return (
     <div className="assignments py-4">
       <div className="container">
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,22 @@
+import AssignmentCard from "../components/AssignmentCard";
 import HeroSection from "../components/home/HeroSection";
-
+import assignments from "../data/assignmentList.json";
 const Home = () => {
   return (
     <div className="home-main">
       <HeroSection />
+      <div className="assignments py-4">
+        <div className="container">
+          <h2>Assignments</h2>
+          <div className="assignment-block mt-4">
+            <div className="row">
+              {assignments.map((card) => (
+                <AssignmentCard key={card.id} item={card} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
